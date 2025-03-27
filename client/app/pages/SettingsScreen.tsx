f079a950d6f3e8ced9829551b8c1716d94f6a0bc
unknown <rasmus.klaaser@voco.ee>
2025-03-27 12:06:03 +0200

    creating settings

--- client/app/pages/SettingsScreen.tsx
+++ client/app/pages/SettingsScreen.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
 export default function SettingsScreen() {
   return (
     <View style={styles.container}>
-      <Text>Settings</Text>
+      <Image 
+        source={require('../../assets/images/SettingsBanner.webp')} 
+        style={styles.image} 
+      />
+      <Text style={styles.text}>BOARDSHOOT</Text>
+      <Text style={styles.text2}>Personal Information</Text>
+      
+      {/* Box that acts as a button */}
+      <TouchableOpacity style={styles.box} onPress={() => console.log("Button pressed!")}>
+        <Text style={styles.boxText}>Rannu68</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -12,8 +22,41 @@ export default function SettingsScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: "#D9EDDF"
   },
+  image: {
+    width: '100%', 
+    height: 200, 
+    resizeMode: 'cover', 
+    position: 'absolute',
+    top: 0,
+  },
+  text: {
+    marginTop: 85, 
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: "#FFFFFF",
+  },
+  text2: {
+    marginTop: 105, 
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: "black",
+    marginRight: 180
+  },
+  box: {
+    width: 300,
+    height: 50, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    backgroundColor: '#C28D00', 
+    marginTop: 30, 
+    borderRadius: 5, 
+  },
+  boxText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  }
 });
-
