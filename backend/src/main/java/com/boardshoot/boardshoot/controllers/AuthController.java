d61d95b2d76f34c33c39c22321a55c8f0d1dcb79
Aleksander P <77441538+ManInBlack-coder@users.noreply.github.com>
2025-04-04 20:16:14 +0300

    did a merge

--- backend/src/main/java/com/boardshoot/boardshoot/controllers/AuthController.java
+++ backend/src/main/java/com/boardshoot/boardshoot/controllers/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
